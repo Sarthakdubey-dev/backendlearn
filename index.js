c4996ad9948e93b37649c4bd4f14279e04da16fe
sarthakD <dsarthak2005@gmail.com>
2026-02-06 14:58:09 +0530

    Update for othertest

--- index.js
+++ index.js
@@ -53,9 +53,6 @@ app.get('/login', (req, res) => {
     res.send('<h1>Login Page</h1>')
 })
 
-app.get('/github', (req, res) => {
-    res.send(githubData)
-})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
